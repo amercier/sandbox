4fb30a99e65838c4eb1b4536ec84d4b843bd875a
amercier <alexandre.mercier12@gmail.com>
2012-07-10 16:55:42 +0200

    Added Charset demos

---
+++ charset/01-none/script.js
@@ -0,0 +1,1 @@
+document.getElementById('js-external').innerHTML = '✰✰✰ ' + document.getElementById('js-external').innerHTML + ' ✰✰✰';
---
+++ charset/04-http-header/readme.txt
@@ -0,0 +1,9 @@
+Apache configuration
+====================
+
+AllowOverride All
+
+
+
+
+sudo a2enmod expires
