354cd538b7fb47c46e9c8d9c34f7494abb4a2c21
amercier <alexandre.mercier12@gmail.com>
2012-07-10 15:04:05 +0200

    Added domReady!

--- amd/02 - Full/main.js
+++ amd/02 - Full/main.js
@@ -16,7 +16,7 @@ function echo(message) {
 console.log('Loading main.js');
 echo('Loading main.js');
 
-define(['module1', 'module2', 'module3', 'module4'], function(module1, module2, module3, module4){
+define(['module1', 'module2', 'module3', 'module4', 'domReady!'], function(module1, module2, module3, module4){
 	
 	console.log('Executing main.js');
 	echo('Executing main.js');
