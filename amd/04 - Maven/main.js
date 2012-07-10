806eaa485bd21b87d8b99ddf886ad412d0d50c02
amercier <alexandre.mercier12@gmail.com>
2012-07-10 15:04:48 +0200

    Added domReady!

--- amd/04 - Maven/main.js
+++ amd/04 - Maven/main.js
@@ -3,7 +3,7 @@
 console.log('Loading main.js');
 echo('Loading main.js');
 
-define(['module1', 'module2', 'module3', 'module4'], function(module1, module2, module3, module4){
+define(['module1', 'module2', 'module3', 'module4', 'domReady!'], function(module1, module2, module3, module4){
 	
 	console.log('Executing main.js');
 	echo('Executing main.js');
