668ce9566a00df8bcb62ad2556913ba9217af6df
amercier <alexandre.mercier12@gmail.com>
2012-07-13 08:12:44 +0200

    Fixed issues with domReady!

--- amd/04-maven/compile.sh
+++ amd/04-maven/compile.sh
@@ -1,2 +1,11 @@
-node ../lib/r-2.0.2.js -o include=../lib/require-2.0.2.js name=main out=main.r.min.js baseUrl=.
-java -jar ../lib/compiler.jar --compilation_level=ADVANCED_OPTIMIZATIONS --js ../lib/require-2.0.2.js main.js  --js_output_file main.cc.min.js
+node ../lib/r-2.0.2.js -o \
+	include=../lib/require-2.0.2 \
+	paths.domReady=../lib/domReady \
+	name=main \
+	out=./build/main.r.min.js \
+	baseUrl=.
+
+java -jar ../lib/compiler.jar \
+	--compilation_level=ADVANCED_OPTIMIZATIONS \
+	--js ../lib/require-2.0.2.js ../lib/domReady.js main.js  \
+	--js_output_file ./build/main.cc.min.js
