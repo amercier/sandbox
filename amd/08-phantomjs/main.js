eb7dd96997e19cf015a9f04b82ac400380ff5a7b
amercier <alexandre.mercier12@gmail.com>
2012-10-05 10:29:00 +0200

    Fixed amd/phantomjs

--- amd/08-phantomjs/main.js
+++ amd/08-phantomjs/main.js
@@ -10,7 +10,6 @@ if(!('log' in console)) {
 require = null;
 importScripts = phantom.injectJs;
 phantom.injectJs('require-2.1.0-phantomjs.js');
-//phantom.injectJs('../lib/require-2.1.0.js');
 require.config({
 	paths: {
 		'domReady': '../../lib/domReady'
@@ -23,7 +22,4 @@ console.log('Loading main.js');
 require(['module2', 'domReady!'], function(module2){
 	console.log('Executing main.js');
 	phantom.exit();
-}/*, function(e) {
-	//throw e.originalError.originalError.originalError;
-	test = tupeuxpastest;
-}*/);
+});
