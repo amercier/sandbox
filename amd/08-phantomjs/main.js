34adcd94a40c2d92030f25bdfa30a01d0b760fca
amercier <alexandre.mercier12@gmail.com>
2012-10-08 10:45:07 +0200

    Renamed main2.js to main.js

--- amd/08-phantomjs/main.js
+++ amd/08-phantomjs/main.js
@@ -7,127 +7,55 @@ if(!('log' in console)) {
 	console.log = function(message) {};
 }
 
-//Overrides the setTimeoutMethod
 !function() {
-	var originalSetTimeout = setTimeout;
-	setTimeout = function(fn, delay) {
-		var timeoutId = originalSetTimeout(function(){},0);
-		fn && typeof fn === 'function' && fn();
-		return timeoutId;
-	};
-}();
-
-require = null;
-importScripts = phantom.injectJs;
-phantom.injectJs('../lib/require-2.1.0.js');
-
-/*
-var oldContextCreator = requirejs.s.newContext;
-requirejs.s.newContext = function(name) {
-	var context = oldContextCreator(name);
-	console.log('Creating context', name, context);
-	return context;
-};
-*/
-
-
-var oldMethods = {};
-['require','load','toUrl','exec','nextTick','execCb','completeLoad'].forEach(function(i) {
-	if(requirejs.s.contexts._.hasOwnProperty(i) && typeof requirejs.s.contexts._[i] === 'function' && !(i in oldMethods)) {
-		console.log('Overriding ' + i);
-		oldMethods[i] = requirejs.s.contexts._[i];
-		requirejs.s.contexts._[i] = function() {
-			console.log('    => ' + i + '(' + /*Array.prototype.join.call(arguments,' , ')*/arguments.length + ')');
-			for(var j in this) {
-				if((j in this) && typeof this[j] !== 'function') {
-					console.log('                ' + j + ' => ' + this[j]);
-				}
-			}
-			for(var j in this.defined) {
-				if((j in this.defined) && typeof this.defined[j] !== 'function') {
-					console.log('                ' + j + ' >> ' + this.defined[j]);
-				}
-			}
-			for(var j in this.urlFetched) {
-				if((j in this.urlFetched) && typeof this.urlFetched[j] !== 'function') {
-					console.log('                ' + j + ' :> ' + this.urlFetched[j]);
-				}
-			}
-			
-			oldMethods[i].apply(this.arguments);
-		};
-	}
-});
-
-require = requirejs;
-
-/*
-require.s.contexts._.nextTick = function(fn) {
-	console.log('nextTick', fn);
-	fn();
-};
-*/
-
-/*
-require = function() {
-	//console.log('REQUIRE (' + Array.prototype.join.call(arguments,' , ') + ')');
-	requirejs.apply(this, arguments);
-};
-*/
-
-requirejs.config({
-	paths: {
-		'domReady': '../../lib/domReady'
-	},
-	callback: phantom.exit
-});
-
-requirejs.onError = function(err, isOriginalError) {
-	if(!isOriginalError) {
-		console.log('##################### ' + err.message + (err.requireModules ? ' in ' + err.requireModules : '') + ' #####################');
-	}
-	for(var i in err) {
-		if((i in err) && typeof err[i] !== 'function') {
-			console.log('    ' + i + ' => ' + err[i]);
+	
+	require = {
+		callback: function() {
+			phantom.exit();
+		},
+		paths: {
+			domReady: '../lib/domReady'
 		}
-	}
-};
+	};
+	importScripts = phantom.injectJs;
+	phantom.injectJs('../lib/require-2.1.0.js');
+	require.load = function(context, moduleName, url) {
+		if(phantom.injectJs(url)) {
+			context.completeLoad(moduleName);
+		}
+		else {
+			throw new Error('Can\'t load ' + url);
+		}
+	};
+	require.onError = function(err) {
+		console.log(err.requireType);
+		if (err.requireType === 'timeout') {
+			console.log('modules: ' + err.requireModules);
+		}
+		throw err;
+	};
+	
+	require([], function(module2){
+		console.log('┌───────────────────────────────┐');
+		console.log('│ ✔ Running main1 (w/o any dep) │');
+		console.log('└───────────────────────────────┘');
+	});
 
-console.log('Including RequireJs');
-console.log('Loading main.js');
+	require(['module1'], function(module1){
+		console.log('┌──────────────────────────────────────┐');
+		console.log('│ ✔ Running main2 (1 dep w/o domready) │');
+		console.log('└──────────────────────────────────────┘');
+	});
 
-require([], function(module2){
-	console.log('┌───────────────────────────────┐');
-	console.log('│ ✔ Running main1 (w/o any dep) │');
-	console.log('└───────────────────────────────┘');
-});
+	require(['module2'], function(module2){
+		console.log('┌───────────────────────────────────────┐');
+		console.log('│ ✔ Running main2 (2 deps w/o domready) │');
+		console.log('└───────────────────────────────────────┘');
+	});
 
-require(['module2'], function(module2){
-	console.log('┌─────────────────────────────────┐');
-	console.log('│ ✔ Running main2 (w/o domready) │');
-	console.log('└─────────────────────────────────┘');
-});
-
-require(['module2', 'domReady!'], function(module2){
-	console.log('┌─────────────────────────────────┐');
-	console.log('│ ✔ Running main3 (with domready) │');
-	console.log('└─────────────────────────────────┘');
-});
-
-console.log(requirejs.s.contexts._.defQueue.length + ' modules to load');
-console.log('Loaded modules:');
-for(var i in requirejs.s.contexts._.defined) {
-	if(requirejs.s.contexts._.defined.hasOwnProperty(i)) {
-		console.log('  - ' + i + ': ' + (typeof requirejs.s.contexts._.defined[i]));
-	}
-}
-console.log('Queue:');
-for(var i in requirejs.s.contexts._.defQueue) {
-	if(requirejs.s.contexts._.defQueue.hasOwnProperty(i)) {
-		console.log('  - ' + i + ': ' + (typeof requirejs.s.contexts._.defQueue[i]));
-	}
-}
-
-setTimeout(function() {
-	phantom.exit();
-}, 5000);
+	require(['module2', 'domReady!'], function(module2){
+		console.log('┌─────────────────────────────────┐');
+		console.log('│ ✔ Running main3 (with domready) │');
+		console.log('└─────────────────────────────────┘');
+	});
+}();
--- amd/08-phantomjs/run.sh
+++ amd/08-phantomjs/run.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-#echo "$ Running: phantomjs timeout.js"
-#phantomjs timeout.js
-#echo "$ Done"
-
 echo "$ Running: phantomjs main.js"
-#phantomjs main.js
 #phantomjs --debug=yes --remote-debugger-port=9000 --remote-debugger-autorun=yes main.js
-phantomjs main2.js
+phantomjs main.js
 echo "$ Done"
