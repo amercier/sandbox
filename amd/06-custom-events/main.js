c040fa9207eba0eced29866f1f6d6c00450d74e6
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-06-05 15:38:22 +0200

    Fixed missing echo method

--- amd/06-custom-events/main.js
+++ amd/06-custom-events/main.js
@@ -1,5 +1,18 @@
 "use strict";
 
+if(!('console' in window)) {
+	console = {};
+}
+
+if(!('log' in console)) {
+	var c = document.getElementById('console');
+	console.log = function(message) {};
+}
+
+function echo(message) {
+	document.getElementById('console').appendChild(document.createTextNode(message + "\n\r"));
+}
+
 console.log('Loading main.js');
 echo('Loading main.js');
 
