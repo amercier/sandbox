a9dc9824e88225a90b7623853ace37ad8bc5b98a
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2013-09-27 15:42:01 +0200

    WIP on ZF2 Getting Started - Forms and Actions

--- php/zf2/01-getting-started/module/Album/src/Album/Model/Album.php
+++ php/zf2/01-getting-started/module/Album/src/Album/Model/Album.php
@@ -2,6 +2,11 @@
 
 namespace Album\Model;
 
+// Add these import statements
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface;
+
 /**
  * For this tutorial, we are going to create a very simple model by creating an
  * AlbumTable class that uses the Zend\Db\TableGateway\TableGateway class in
@@ -12,8 +17,14 @@ namespace Album\Model;
  * a temptation to put database access code into controller action methods as
  * these are exposed by Zend\Db\TableGateway\AbstractTableGateway. Don’t do
  * this!
+ *
+ * We also need to set up validation for this form. In Zend Framework 2 this is
+ * done using an input filter, which can either be standalone or defined within
+ * any class that implements the InputFilterAwareInterface interface, such as a
+ * model entity. In our case, we are going to add the input filter to the Album
+ * class, which resides in the Album.php file in module/Album/src/Album/Model
  */
-class Album
+class Album implements InputFilterAwareInterface
 {
     public $id;
     public $artist;
@@ -31,5 +42,73 @@ class Album
         $this->id     = (!empty($data['id']))     ? $data['id']     : null;
         $this->artist = (!empty($data['artist'])) ? $data['artist'] : null;
         $this->title  = (!empty($data['title']))  ? $data['title']  : null;
+    }
+
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+        throw new \Exception("Not used");
+    }
+
+    public function getInputFilter()
+    {
+        if (!$this->inputFilter) {
+
+            $inputFilter = new InputFilter();
+
+            $inputFilter->add(array(
+                'name'     => 'id',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int'),
+                ),
+            ));
+
+            $inputFilter->add(array(
+                'name'     => 'artist',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            ));
+
+            $inputFilter->add(array(
+                'name'     => 'title',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            ));
+
+            $this->inputFilter = $inputFilter;
+        }
+
+        return $this->inputFilter;
+    }
+
+    public function getArrayCopy()
+    {
+        return get_object_vars($this);
     }
 }
