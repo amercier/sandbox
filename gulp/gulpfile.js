f4892d78b7eeb972a7f73ef677283f7f11f23b3d
Alexandre Mercier <pro.alexandre.mercier@gmail.com>
2014-10-15 14:30:37 -0700

    Added 'gulp sass-dev'

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var cssmin = require('gulp-cssmin');
 
 // uglify: {
 //   base: {
@@ -19,7 +22,7 @@ var uglify = require('gulp-uglify');
 //         'bower_components/foundation/js/foundation/foundation.tab.js'
 //       ]
 //     }
-//   },
+//   }
 gulp.task('uglify-base', function() {
 
   gulp.src([
@@ -31,13 +34,36 @@ gulp.task('uglify-base', function() {
     'lib/foundation/js/foundation/foundation.tab.js'
   ]).pipe(concat('foundation.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('public/js/'))
+    .pipe(gulp.dest('public/js/'));
 
   gulp.src([
     'lib/modernizr/modernizr.js'
   ])
     .pipe(concat('modernizr.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('public/js/'))
+    .pipe(gulp.dest('public/js/'));
 
+});
+
+// sass: {
+//   dev: {
+//     options: {
+//       loadPath: ['lib/foundation/scss'],
+//       style: 'compressed'
+//     },
+//     files: {
+//       'public/css/app.css': 'scss/app.min.scss'
+//     }
+//   }
+// }
+gulp.task('sass-dev', function() {
+  gulp.src([
+    'scss/app.scss',
+    'scss/typography.scss'
+  ]).pipe(sass({
+      includePaths: ['lib/foundation/scss']
+    }))
+    .pipe(cssmin())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('public/css/'));
 });
